e2966bc8db9648a1fbe7b411536afda798a76f69
lukgu <szukasz.gutowski@gmail.com>
2016-07-14 15:59:22 +0200

    rar zadziałał

---
+++ 7-6-grunt/Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'main.css': 'main.sass'
+  			}
+  		}
+  	},
+
+  	imagemin: {
+  		dynamic: {
+  			files: [{
+  				expand: true,
+  				cwd: 'images/',
+  				src: ['**/*.{png,jpg,gif}'],
+  				dest: 'images/build/'
+  			}]
+  		}
+  	},
+
+  	watch: {
+    scripts: {
+        files: ['css/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        }
+    	} 
+	},
+
+	browserSync: {
+    	bsFiles: {
+        src : 'assets/css/*.css'
+    	},
+    	options: {
+       	 server: {
+            baseDir: "./"
+        	}
+    	}
+	}
+
+  });
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+  // Default task(s).
+  
+  grunt.registerTask('default', ['sass', 'imagemin' , 'browserSync']);
+};
